65fc554da80fb22400cfffabc58ef99d280d7139
Matty <mlandels@tractionondemand.com>
2020-08-18 18:21:36 -0700

    The Good Stuff

---
+++ nightclub/index.js
@@ -0,0 +1,29 @@
+let nightclubName = 'Tom and Matts Nightclub';
+let members = [
+	{
+		'Name' : 'Jimmy Napkins',
+		'Age' : 24,
+		'Height' : '6\'0"'
+	},
+	{
+		'Name' : 'Billy Shakes',
+		'Age' : 18,
+		'Height' : '5\'4"'
+	},
+	{
+		'Name' : 'Ted Coffee',
+		'Age' : 24,
+		'Height' : '5\'11"'
+	}
+];
+
+
+(function () {
+
+    ConsoleMembers = function () {
+        console.error(members);
+    };
+
+    ConsoleMembers();
+
+})();
