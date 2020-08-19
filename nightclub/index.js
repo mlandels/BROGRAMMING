ea9794be4c98d396fe61c3e0db33444b14c1bb33
Spicytom <tom.adam21@gmail.com>
2020-08-18 19:42:02 -0700

    validate vip and age
    bottles!

--- nightclub/index.js
+++ nightclub/index.js
@@ -3,27 +3,41 @@ let members = [
 	{
 		'Name' : 'Jimmy Napkins',
 		'Age' : 24,
-		'Height' : '6\'0"'
+		'Height' : '6\'0"',
+		'Vip' : 'yes'
 	},
 	{
 		'Name' : 'Billy Shakes',
 		'Age' : 18,
-		'Height' : '5\'4"'
+		'Height' : '5\'4"',
+		'Vip' : 'no'
 	},
 	{
 		'Name' : 'Ted Coffee',
 		'Age' : 24,
-		'Height' : '5\'11"'
+		'Height' : '5\'11"',
+		'Vip' : 'no'
 	}
 ];
 
 
-(function () {
 
-    ConsoleMembers = function () {
-        console.error(members);
-    };
+    validateAge = function () {
+			members.forEach(function(m) {
+				if (m.Age > 18) {
+					console.log(m)
+				}
+			 })
+		};
+	
+let validateVip = function () {
+	members.forEach(function(v) {
+		if (v.Vip === 'yes') {
+			console.log('you get a bottle bruh');
+		} else {
+			console.log('no bottle for you');
+		}
+	})
+}
 
-    ConsoleMembers();
-
-})();
+validateVip();
